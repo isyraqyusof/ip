94132c1fd2842b72b8c480326e734c7ff02d8960
isyraq <misyraq2003@gmail.com>
2025-08-22 10:07:32 +0800

    implemented skeletal version of chatbot

---
+++ src/main/java/Cookie.java
@@ -0,0 +1,11 @@
+public class Cookie {
+    public static void main(String[] args) {
+        String line = "___________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm Cookie");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
