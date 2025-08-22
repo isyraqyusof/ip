018609c9d6e1345665f0a256d7a0879a1e5e24a8
isyraq <misyraq2003@gmail.com>
2025-08-22 12:55:07 +0800

    added support for tracking todos, deadlines and events

--- src/main/java/Cookie.java
+++ src/main/java/Cookie.java
@@ -13,7 +13,8 @@ public class Cookie {
 
         while (true) {
             String fullInput = scanner.nextLine();
-            String[] splitInput = fullInput.split(" ");
+            String[] splitInput = fullInput.split(" ", 2);
+            String firstWord = splitInput[0];
             if (fullInput.equals("bye")) {
                 System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
@@ -30,7 +31,7 @@ public class Cookie {
                 }
                 System.out.println(line);
             }
-            else if (splitInput[0].equals("mark")) {
+            else if (firstWord.equals("mark")) {
                 int indexToBeMarked = Integer.parseInt(splitInput[1]) - 1;
                 listOfTasks[indexToBeMarked].markAsDone();
                 System.out.println(line);
@@ -38,12 +39,50 @@ public class Cookie {
                 System.out.println(listOfTasks[indexToBeMarked]);
                 System.out.println(line);
             }
-            else if (splitInput[0].equals("unmark")) {
+            else if (firstWord.equals("unmark")) {
                 int indexToBeUnmarked = Integer.parseInt(splitInput[1]) - 1;
                 listOfTasks[indexToBeUnmarked].markAsUndone();
                 System.out.println(line);
                 System.out.println("Alright. I've marked this task as not done yet:");
                 System.out.println(listOfTasks[indexToBeUnmarked]);
+                System.out.println(line);
+            }
+            else if (firstWord.equals("todo")) {
+                Task newTodo = new Todo(splitInput[1]);
+                listOfTasks[counter] = newTodo;
+                counter++;
+                System.out.println(line);
+                System.out.println("A todo, got it! I've added this task:");
+                System.out.println(newTodo);
+                System.out.println("Now you have " + counter + " tasks in the list.");
+                System.out.println(line);
+            }
+            else if (firstWord.equals("deadline")) {
+                String[] secondPhraseSplit = splitInput[1].split("/by");
+                String description = secondPhraseSplit[0];
+                String deadline = secondPhraseSplit[1];
+                Task newDeadline = new Deadline(description, deadline);
+                listOfTasks[counter] = newDeadline;
+                counter++;
+                System.out.println(line);
+                System.out.println("A deadline, got it! I've added this task:");
+                System.out.println(newDeadline);
+                System.out.println("Now you have " + counter + " tasks in the list.");
+                System.out.println(line);
+            }
+            else if (firstWord.equals("event")) {
+                String[] secondPhraseSplit = splitInput[1].split("/from");
+                String[] thirdPhraseSplit = secondPhraseSplit[1].split("/to");
+                String description = secondPhraseSplit[0];
+                String from = thirdPhraseSplit[0];
+                String to = thirdPhraseSplit[1];
+                Task newEvent = new Event(description, from, to);
+                listOfTasks[counter] = newEvent;
+                counter++;
+                System.out.println(line);
+                System.out.println("An event, got it! I've added this task:");
+                System.out.println(newEvent);
+                System.out.println("Now you have " + counter + " tasks in the list.");
                 System.out.println(line);
             }
             else {
