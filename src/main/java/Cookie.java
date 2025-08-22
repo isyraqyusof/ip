e792186c5a103e4d6dbe6fc6c270f6f0df107e1b
isyraq <misyraq2003@gmail.com>
2025-08-22 10:49:34 +0800

    added echoing functionality

--- src/main/java/Cookie.java
+++ src/main/java/Cookie.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
 public class Cookie {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String line = "___________________________________";
         System.out.println(line);
         System.out.println("Hello! I'm Cookie");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            else {
+                System.out.println(line);
+                System.out.println(input);
+                System.out.println(line);
+            }
+        }
     }
 }
